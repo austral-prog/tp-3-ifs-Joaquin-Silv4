d29a4aca8a946da555b52c03e1e854a5910f52b5
Joaquin-Silv4 <Jsilva3@mail.austral.edu.ar>
2026-04-07 12:19:06 -0300

    Update exercise_grades.py

--- exercise_grades.py
+++ exercise_grades.py
@@ -19,3 +19,24 @@ def grades():
         Insuficiente
     """
     pass
+
+    nota=int(input("Ingrese nota del 1 al 10:"))
+
+    if 9 <= nota <= 10:
+        print("Excelente")
+
+    elif 7 <= nota <= 8:
+        print("Bueno")
+
+    elif 5 <= nota <= 6:
+        print("Regular")
+
+    elif 0 <= nota <= 4:
+        print("Insuficiente")
+
+    else:
+        print("Nota fuera de rango")
+
+
+
+grades()
