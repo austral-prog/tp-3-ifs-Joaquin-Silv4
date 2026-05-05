2804c12b91830d5d38c717f8c4a83751b1f0212b
Joaquin-Silv4 <Jsilva3@mail.austral.edu.ar>
2026-05-05 11:21:49 -0300

    Update exercise_leap_year.py

--- exercise_leap_year.py
+++ exercise_leap_year.py
@@ -19,7 +19,7 @@ def leap_year():
     """
     pass
 
-    ano=int(input("Ingrese año:"))
+    ano=int(input())
 
     if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
         print(f"El año {ano} es bisiesto")
