9a45bce766db513a849c68a5fd2e4fe5f06a2b9c
Joaquin-Silv4 <Jsilva3@mail.austral.edu.ar>
2026-05-21 17:35:01 -0300

    Update exercise_grades.py

--- exercise_grades.py
+++ exercise_grades.py
@@ -18,7 +18,7 @@ def grades():
         Para la entrada "3", la salida esperada es:
         Insuficiente
     """
-    pass
+    
 
     nota=int(input())
 
@@ -39,4 +39,4 @@ def grades():
 
 
 
-grades()
+
