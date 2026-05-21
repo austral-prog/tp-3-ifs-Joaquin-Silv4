dd9300cfbb606d6b7f91e25c3d5a42576075690c
Joaquin-Silv4 <Jsilva3@mail.austral.edu.ar>
2026-05-21 18:05:34 -0300

    Update exercise_age_check.py

--- exercise_age_check.py
+++ exercise_age_check.py
@@ -23,7 +23,7 @@ def age_check():
     limite_edad=int(input())
 
     if edad <= 0 or limite_edad <= 0:
-    print("Entrada invalida")
+        print("Entrada invalida")
 
     elif edad < limite_edad:
         print("Eres menor de edad")
